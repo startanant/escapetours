0a894914d9a1fd7cd136382aaeb82daaab304d0a
Przemyslaw Rudzki <przemek.rudzki@gmail.com>
2020-02-12 11:51:02 -0500

    changing js script sequence, plugging randomCity function to maps

--- assets/map.js
+++ assets/map.js
@@ -2,9 +2,16 @@
 //call this function with latitude, longitude co-ordinates to insert map into the target div
 
 //for example:
-paris_lat = 2.35236;
-paris_lng = 48.856461;
-generate_map(paris_lat, paris_lng);
+// paris_lat = 2.35236;
+// paris_lng = 48.856461;
+// generate_map(paris_lat, paris_lng);
+
+let coord = p_getRandomCity('Europe');
+
+console.log(coord.city);
+console.log(coord.coord.lat, coord.coord.lon);
+
+generate_map(coord.coord.lon,coord.coord.lat);
 
 function generate_map(lat, lng) {
     var map = new ol.Map({
@@ -16,7 +23,7 @@ function generate_map(lat, lng) {
         ],
         view: new ol.View({
           center: ol.proj.fromLonLat([lat,lng]),
-          zoom: 8
+          zoom: 10
         })
     });
 }
