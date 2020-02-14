13ae1d17bdc1ec2356844dd5dd13bcdeb09779ba
Anant S <startanant@gmail.com>
2020-02-14 14:51:46 -0500

    Fix languages element in facts

--- assets/facts.js
+++ assets/facts.js
@@ -3,7 +3,7 @@ function getFacts(country) {
     
     document.getElementById('country_name').innerText = "";
     document.getElementById('population').innerText = "";
-    document.getElementById('background').innerText = "";
+    document.getElementById('area').innerText = "";
     document.getElementById('languages').innerText = "";
     
     $.getJSON("https://raw.githubusercontent.com/iancoleman/cia_world_factbook_api/master/data/factbook.json", function(countries) {
@@ -21,8 +21,9 @@ function getFacts(country) {
     
     document.getElementById('country_name').innerText = country_name;
     document.getElementById('population').innerText = "Population: "+ population;
-    document.getElementById('area').innerText = area;
+    document.getElementById('area').innerText = "Area: "+area + "sq km";
     document.getElementById('languages').innerText = "Languages: " +languages_list;
+    languages_list = '';
 
 
 
@@ -37,4 +38,5 @@ function logLanguage(element, index, array) {
     }
     
     return languages_list;
+    
 }
